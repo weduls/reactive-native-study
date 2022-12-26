b2825c6f9fb144b6553bd9b1459f2b5cbdf03788
wedul <wedul.chul@gmail.com>
2022-12-27 01:45:53 +0900

    font, image

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Dimensions, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {MD2Colors} from 'react-native-paper';
+import {Image, ImageBackground, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import * as D from './src/data';
 
 // chapter 3
 // const person = D.makeArray(100).map(D.createRandomPerson)
@@ -42,36 +42,74 @@ import {MD2Colors} from 'react-native-paper';
 // }
 
 
-// 현재 폰 사이즈
-const {width, height} = Dimensions.get('window');
-// 현재 실행중인 OS확인
-let platform = Platform.OS;
-export default function App() {
-    return (<SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
-        <Text style={[styles.text, {color: 'white'}]}>OS: {platform}</Text>
-        <Text style={[styles.text, {color: 'white'}]}>width: {width}px</Text>
-        <Text style={[styles.text, {color: 'white'}]}>height: {height}px</Text>
+// chapter 4 - style
+// // 현재 폰 사이즈
+// const {width, height} = Dimensions.get('window');
+// // 현재 실행중인 OS확인
+// let platform = Platform.OS;
+// export default function App() {
+//     return (<SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
+//         <Text style={[styles.text, {color: 'white'}]}>OS: {platform}</Text>
+//         <Text style={[styles.text, {color: 'white'}]}>width: {width}px</Text>
+//         <Text style={[styles.text, {color: 'white'}]}>height: {height}px</Text>
+//
+//         <View style={[styles.box, styles.border]}></View>
+//         <View style={[styles.box, styles.border, {borderRadius: 20}]}></View>
+//         <View style={[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}></View>
+//     </SafeAreaView>);
+// }
+//
+//
+//
+// // StyleSheet를 만들어서 사용하는게 더 효율적. 인라인으로 그냥 사용할 경우 매번 그려질때 마다 style객체가 생성되어 느려짐
+// // 우선순위가 width, height가 flex보다 높다.
+// // margin은
+// const styles = StyleSheet.create({
+//     safeAreaView: {
+//         backgroundColor: MD2Colors.blue500,
+//         flex: 1,
+//         paddingLeft: Platform.select({ios: 0, android: 20})
+//     },
+//     text: {marginBottom: 10, marginLeft: 50, marginTop: 20, fontSize: 20, color: MD2Colors.blue200},
+//     box: {height: 100, width: '70%', backgroundColor: 'white', marginBottom: 10,
+//         marginLeft: Platform.select({ios: 20, android: 0})
+//     },
+//     border: {borderWidth: 10, borderColor: MD2Colors.blue900}
+// });
 
-        <View style={[styles.box, styles.border]}></View>
-        <View style={[styles.box, styles.border, {borderRadius: 20}]}></View>
-        <View style={[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}></View>
-    </SafeAreaView>);
+
+// chapter 4 - font, image
+const text = 'Almost before we knew it, we had left the ground'
+export default function App() {
+    return<SafeAreaView style={styles.flex}>
+        <ImageBackground
+            style={[styles.backgroundImage]}
+            source={require('./src/assets/images/bg.jpg')}>
+            <Image source={{uri: avatarUrl}} style={[styles.image]} />
+            <View style={[styles.flex, styles.padding10]}>
+                <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+                <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+                <Text style={[styles.text, styles.semiBold]}>{text} [semiBold]</Text>
+                <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+            </View>
+        </ImageBackground>
+    </SafeAreaView>
 }
 
-
-
-// StyleSheet를 만들어서 사용하는게 더 효율적. 인라인으로 그냥 사용할 경우 매번 그려질때 마다 style객체가 생성되어 느려짐
-// 우선순위가 width, height가 flex보다 높다.
-// margin은
+const avatarSize = 50;
+const avatarUrl = D.randomAvatarUrl()
 const styles = StyleSheet.create({
-    safeAreaView: {
-        backgroundColor: MD2Colors.blue500,
-        flex: 1,
-        paddingLeft: Platform.select({ios: 0, android: 20})
-    },
-    text: {marginBottom: 10, marginLeft: 50, marginTop: 20, fontSize: 20, color: MD2Colors.blue200},
-    box: {height: 100, width: '70%', backgroundColor: 'white', marginBottom: 10,
-        marginLeft: Platform.select({ios: 20, android: 0})
-    },
-    border: {borderWidth: 10, borderColor: MD2Colors.blue900}
-});
+    flex: {flex: 1},
+    backgroundImage: {flex: 1},
+    image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
+    padding10: {padding: 10},
+    text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
+
+    regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
+    medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'},
+    semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'},
+    bold: {
+        fontFamily: 'DancingScript-Bold',
+        fontWeight: Platform.select({ios: '700', android: '600'})
+    }
+})
---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}
+    },
+    assets: ['./src/assets/fonts']
+}
+// native 자원인 font등을 사용할 때 필요한 정의이고 npx react-native link (npx react-native-asset 대체됨) 명령어를 실행해서 적용을 해줘야 반영된다.
