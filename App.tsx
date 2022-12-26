bbeccb2487782ecd01ee852d7ecceab974b49aee
wedul <wedul.chul@gmail.com>
2022-12-27 00:26:03 +0900

    event study

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
-import ClassComponent from './src/screens/ClassComponent';
-import ArrowComponent from './src/screens/ArrowComponent';
-import * as D from './src/data'
-import Person from './src/screens/Person';
+import {Alert, Button, SafeAreaView, Text, TextInput, TouchableHighlight, TouchableOpacity} from 'react-native';
 
-const person = D.makeArray(100).map(D.createRandomPerson)
+// chapter 3
+// const person = D.makeArray(100).map(D.createRandomPerson)
+// export default function App() {
+//     const children = person.map((person) => (
+//         // key는 랜더링시 필요함
+//         <Person key={person.id} person={person}/>
+//     ))
+//
+//     return (
+//         <SafeAreaView>
+//             <ScrollView>{children}</ScrollView>
+//         </SafeAreaView>
+//     )
+// }
+
 export default function App() {
-    const children = person.map((person) => (
-        // key는 랜더링시 필요함
-        <Person key={person.id} person={person}/>
-    ))
-
+    let onPress = () => Alert.alert('home pressed', 'message');
     return (
         <SafeAreaView>
-            <ScrollView>{children}</ScrollView>
+            <Button title='Home' onPress={onPress} />
+            {/*// TouchableOpacity가 조금더 은은한게 이벤트가 발생*/}
+            <TouchableOpacity onPress={onPress}>
+                <Text>TouchableOpacity</Text>
+            </TouchableOpacity>
+            <TouchableHighlight onPress={onPress}>
+                <Text>TouchableHighLight</Text>
+            </TouchableHighlight>
+            <TextInput placeholder="enter your name"
+                       onChangeText={(text:string) => console.log(text)}
+                       onFocus={() => console.log('onFocus')}
+                       onBlur={() => console.log('onBlur')}
+                       onEndEditing={() => console.log('onEndEditing')}
+           ></TextInput>
         </SafeAreaView>
+
     )
 }
