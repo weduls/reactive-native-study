527b115788bd799427edb284942d03d66849dfab
wedul <wedul.chul@gmail.com>
2022-12-28 15:27:31 +0900

    hock 기초

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {Fragment} from 'react';
-import {Alert, FlatList, Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {Fragment, useEffect, useState} from 'react';
+import {Alert, FlatList, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {MD2Colors} from 'react-native-paper';
 import TopBar from './src/screens/TopBar';
 import Content from './src/screens/Content';
@@ -7,6 +7,7 @@ import BottomBar from './src/screens/Bottom';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as D from './src/data';
 import Person from './src/copy/Person';
+import {useClock} from './src/hooks';
 
 // chapter 3
 // const person = D.makeArray(100).map(D.createRandomPerson)
@@ -154,20 +155,38 @@ import Person from './src/copy/Person';
 // });
 
 // chapter 6. component style
-const people: D.IPerson[] = D.makeArray(10).map(D.createRandomPerson);
-export default function App() {
-    return (
-        <SafeAreaView style={styles.flex}>
-            <FlatList data={people} renderItem={({item}) => <Person person={item} />}
-                      keyExtractor={(item, index) => item.id}
-                      ItemSeparatorComponent={() => <View style={styles.itemSeparator}/>} />
-        </SafeAreaView>
-    );
-}
+// const people: D.IPerson[] = D.makeArray(10).map(D.createRandomPerson);
+// export default function App() {
+//     return (
+//         <SafeAreaView style={styles.flex}>
+//             <FlatList data={people} renderItem={({item}) => <Person person={item} />}
+//                       keyExtractor={(item, index) => item.id}
+//                       ItemSeparatorComponent={() => <View style={styles.itemSeparator}/>} />
+//         </SafeAreaView>
+//     );
+// }
+//
+// const styles = StyleSheet.create({
+//     flex: {flex: 1},
+//     itemSeparator: {
+//         borderWidth: 1, borderColor: MD2Colors.grey500,
+//     },
+// });
 
-const styles = StyleSheet.create({
-    flex: {flex: 1},
-    itemSeparator: {
-        borderWidth: 1, borderColor: MD2Colors.grey500,
-    },
-});
+// chapter 7. 리액트 훅
+// export default function App() {
+//     const time = useClock()
+//     return (
+//         <SafeAreaView style={styles.safeAreaView}>
+//             <Text style={[styles.digitFont, styles.time]}>
+//                 {time.toLocaleTimeString()}
+//             </Text>
+//             <Text style={styles.digitFont}>{time.toLocaleDateString()}</Text>
+//         </SafeAreaView>
+//     )
+// }
+// const styles = StyleSheet.create({
+//     safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+//     digitFont: {fontFamily: 'DancingScript-SemiBold', fontWeight: '400'},
+//     time: {fontSize: 50}
+// })
